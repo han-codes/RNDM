6a0bee7ec1b1e7998192555241efaf42a34e7c02
Hannie Kim <hanniek00@gmail.com>
2018-11-14 13:59:25 -0600

    Initial Commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Hannie Kim on 11/14/18.
+//  Copyright © 2018 Hannie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
