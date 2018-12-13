c6eb5e5059f641d79535f4946a0e27b4a2065a02
Hannie Kim <hanniek00@gmail.com>
2018-12-13 07:16:04 -0600

    Move reusable code that gets data from Firestore into a separate method

--- RNDM/Controller/MainVC.swift
+++ RNDM/Controller/MainVC.swift
@@ -63,28 +63,34 @@ class MainVC: UIViewController {
     
     // When database changes, query Category field and update the tableview
     func setListener() {
-        thoughtsListener = thoughtsCollectionRef.whereField(CATEGORY, isEqualTo: selectedCategory).addSnapshotListener { (snapshot, error) in
-            if let err = error {
-                debugPrint("Error fetching docs: \(err)")
-            } else {
-                // empty the thoughts array
-                self.thoughts.removeAll()
-                
-                guard let snap = snapshot else { return }
-                for document in snap.documents {
-                    let data = document.data()
-                    let username = data[USERNAME] as? String ?? "Anonymous"
-                    let timestamp = data[TIMESTAMP] as? Date ?? Date()
-                    let thoughtTxt = data[THOUGHT_TXT] as? String ?? ""
-                    let numLikes = data[NUM_LIKES] as? Int ?? 0
-                    let numComments = data[NUM_COMMENTS] as? Int ?? 0
-                    let documentId = document.documentID
-                    
-                    // add objects with fetched data into thought sarray
-                    let newThought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
-                    self.thoughts.append(newThought)
-                }
-                self.tableView.reloadData()
+        
+        if selectedCategory == ThoughtCategory.popular.rawValue {
+            thoughtsListener = thoughtsCollectionRef
+                .order(by: NUM_LIKES, descending: true)
+                .addSnapshotListener { (snapshot, error) in
+                    if let err = error {
+                        debugPrint("Error fetching docs: \(err)")
+                    } else {
+                        // empty the thoughts array
+                        self.thoughts.removeAll()
+                        self.thoughts = Thought.parseData(snapshot: snapshot)
+                        self.tableView.reloadData()
+                    }
+            }
+        } else {
+            // fetch the thoughts for the current selectedCategory
+            thoughtsListener = thoughtsCollectionRef
+                .whereField(CATEGORY, isEqualTo: selectedCategory)
+                .order(by: TIMESTAMP, descending: true)
+                .addSnapshotListener { (snapshot, error) in
+                    if let err = error {
+                        debugPrint("Error fetching docs: \(err)")
+                    } else {
+                        // empty the thoughts array
+                        self.thoughts.removeAll()
+                        self.thoughts = Thought.parseData(snapshot: snapshot)
+                        self.tableView.reloadData()
+                    }
             }
         }
     }
--- RNDM/Model/Thought.swift
+++ RNDM/Model/Thought.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Firebase
 
 class Thought {
     
@@ -27,4 +28,25 @@ class Thought {
         self.documentId = documentId
     }
     
+    // method to parse Firestore data to array, that table view will display
+    class func parseData(snapshot: QuerySnapshot?) -> [Thought]{
+        var thoughts = [Thought]()
+        
+        guard let snap = snapshot else { return thoughts }
+        for document in snap.documents {
+            let data = document.data()
+            let username = data[USERNAME] as? String ?? "Anonymous"
+            let timestamp = data[TIMESTAMP] as? Date ?? Date()
+            let thoughtTxt = data[THOUGHT_TXT] as? String ?? ""
+            let numLikes = data[NUM_LIKES] as? Int ?? 0
+            let numComments = data[NUM_COMMENTS] as? Int ?? 0
+            let documentId = document.documentID
+            
+            // add objects with fetched data into thought sarray
+            let newThought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
+            thoughts.append(newThought)
+        }
+        
+        return thoughts
+    }
 }
