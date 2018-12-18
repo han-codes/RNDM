0da85809fea07f2c300d2545d9e0040a5f3b7d99
Hannie Kim <hanniek00@gmail.com>
2018-12-18 06:33:57 -0600

    Firebase Auth to create a user

--- RNDM/Controller/CreateUserVC.swift
+++ RNDM/Controller/CreateUserVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateUserVC: UIViewController {
 
@@ -20,16 +21,49 @@ class CreateUserVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         createUserBtn.layer.cornerRadius = 10
         cancelBtn.layer.cornerRadius = 10
     }
     
+    // Creating the user in Firebase Auth
     @IBAction func createUserTapped(_ sender: Any) {
         
+        guard let email = emailTxt.text,
+            let password = passwordTxt.text,
+            let username = usernameTxt.text else { return }
+            
+        // Creating a user
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                debugPrint("Error creating user: \(error.localizedDescription)")
+            }
+            
+            // add display name for the user
+            let changeRequest = user?.user.createProfileChangeRequest()
+            changeRequest?.displayName = username
+            changeRequest?.commitChanges(completion: { (error) in
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                }
+            })
+            
+            guard let userId = user?.user.uid else { return }
+            // add username and date_created to the user using their uid
+            Firestore.firestore().collection(USERS_REF).document(userId).setData([
+                USERNAME : username,
+                DATE_CREATED : FieldValue.serverTimestamp()
+                ], completion: { (error) in
+                    if let error = error {
+                        debugPrint(error.localizedDescription)
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+            })
+        }
     }
     
     @IBAction func cancelBtnTapped(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
     }
 }
